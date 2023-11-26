85530e1147c30dbe44e4fe9c558ca17402b0ce82
manya1575 <151176930+manya1575@users.noreply.github.com>
2023-11-26 15:06:53 +0530

    Delete sql_connection.py

--- sql_connection.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector as mydb
-con=mydb.connect(user="root",passwd="admin123",host="localhost")
-m=con.cursor()
-m.execute("use gs")
-#m.execute("select * from products")
-m.execute("SELECT products.prod_id, products.name, products.uom_id,products.price_per_unit,uom.uom_name FROM products inner join uom on products.uom_id=uom.uom_id")
-for row in m:
-    print(row)
